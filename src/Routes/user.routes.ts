d61118bce048e95f28b3624f9d08e81cae49a7ed
Rewax <daniel.lorenzen@hotmail.com>
2023-12-04 17:37:54 +0100

    Sequalize forsøg

---
+++ src/Routes/user.routes.ts
@@ -0,0 +1,26 @@
+import User, { UserMap } from '../Model/user';
+import database from '../database';
+
+// GET - users
+router.get('/', async (req: Request, res: Response) => {
+    UserMap(database);
+    const result = await User.findAll();
+    res.status(200).json({ users: result });
+  });
+
+  // GET - users/:id
+router.get('/:id', async (req: Request, res: Response) => {
+    UserMap(database);
+    const id = Number(req.params.id);
+    const result = await User.findByPk(id);
+    res.status(200).json({ user: result });
+  });
+
+  // POST - users
+router.post('/', async (req: Request, res: Response) => {
+    let newUser = req.body as User;
+    UserMap(database);
+    const result = await User.create(newUser);
+    newUser = result.dataValues as User;
+    res.status(201).json({ user: newUser });
+  });
---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import http from 'http';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { Request, Response } from 'express';
+import { port } from './config';
+import usersRoutes from './routes/users.routes';
+
+
+const app = express();
+app.use('/users', usersRoutes);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', async (req: Request, res: Response) => {
+  res.status(200).json({
+    message: 'Hello World'
+  });
+});
+
+app.listen(port, () => {
+  console.log(`API started at http://localhost:${port}`);
+});
